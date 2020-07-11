c099720eaecff90e7a8d4f3012d2df9511a07b27
Shuyu Gong <sg699317@ohio.edu>
2020-07-11 11:52:56 -0400

    pycharm

--- preprocess.py
+++ preprocess.py
@@ -5,15 +5,12 @@ obtain single words extracting features (STFT) and saving them.
 '''
 
 import os
-
+import h5py
 import numpy as np
-
-from argparse import ArgumentParser
-
 import soundfile as sf
 from scipy.signal import stft
-
-import h5py
+from glob import glob
+from argparse import ArgumentParser
 
 
 def audio_to_input(audio_file):
@@ -29,21 +26,40 @@ def audio_to_input(audio_file):
     data_tmp[:,:,0] = log_standardize(data_tmp[:,:,0])
     data_tmp= np.delete(data_tmp, (128), axis=1)
     return data_tmp
-	
+
+def clips_to_features_rand(clips, model, num_random_patches=1):
+    specs = []
+    for clip in clips:
+        for n in range(num_random_patches):
+            specs.append(audio_to_input(clip, 16200))
+    #specs = np.array([pad_spec(spec) for spec in specs])
+    specs = np.array([spec for spec in specs])
+    feats = model.predict(specs)
+    a, b, c, d = np.shape(feats)
+    feats_small = np.zeros((len(clips), b, c, d))
+    for i in range(0, len(feats), num_random_patches):
+        feat_tmp = np.mean(feats[i:i+num_random_patches], axis=0)
+        feats_small[int(i/num_random_patches), ...] = feat_tmp
+    return feats_small.reshape(len(clips), -1)
+
+def remove_short_clips(clips, length=16200):
+    good_idxs = []
+    for i, clip in enumerate(clips):
+        audio = sf.read(clip)
+        audio = audio[0]
+        if len(audio)>=length:
+            good_idxs.append(i)
+    clips=np.array(noise_clips)[good_idxs]
+    return clips
+
 	
 def parse_args():
     parser = ArgumentParser(description='Preprocessing of soundfiles')
 
     parser.add_argument(
         '-data', '--data',
-        type=str, default='./LibriSpeech',
+        type=str, default='./data/LibriSpeech/',
         help='dataset to load'
-    )
-
-    parser.add_argument(
-        '-dest_path', '--dest_path',
-        type=str, default='./LibriSpeech_words',
-        help='destination of processed data'
     )
 
     return parser.parse_args()
@@ -52,6 +68,29 @@ def parse_args():
 if __name__ == '__main__':
     # Parse command-line arguments
     args = parse_args()
+    data_path = args.__getattribute__("data")
+    test_clean = glob(data_path + "test-clean/*/*/*.flac")
+    dev_clean = glob(data_path + "dev-clean/*/*/*.flac")
+    train_clean = glob(data_path + "train-clean-100/*/*/*.flac")
+    test_SSN_3 = glob(data_path + "testSSN/*3.wav")
+    dev_SSN_3 = glob(data_path + "devSSN/*3.wav")
+    train_SSN_3 = glob(data_path + "trainSSN/*3.wav")
+    test_SSN_0 = glob(data_path + "testSSN/*0.wav")
+    dev_SSN_0 = glob(data_path + "devSSN/*0.wav")
+    train_SSN_0 = glob(data_path + "trainSSN/*0.wav")
+    for tests in test_clean:
+        audio = sf.read(tests)
+        audio = audio[0]
+        f, t, seg_stft = stft(audio,
+                              window='hamming',
+                              nperseg=256,
+                              noverlap=128)
+        print(seg_stft)
+        mag_spec = np.abs(seg_stft)
+        print(mag_spec)
+        break
+
+
 
     # splits
     print("Hello!")
