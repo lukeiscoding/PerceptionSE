665c01b9c8f0312a40cf79074853b65dd169d411
Shuyu Gong <sg699317@ohio.edu>
2020-07-13 17:59:26 -0400

    pycharm

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 from models import *
+import numpy as np
 
 if __name__ == '__main__':
     model_name = TFU_model_2
     model = model_name.model()
     model.summary()
-    print("hello world")
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+    model.fit()
