4b4ba0e40b0f72b0ec6fb5db160afa8ef31697ed
Shuyu Gong <sg699317@ohio.edu>
2020-07-16 17:51:05 -0400

    pycharm

--- train.py
+++ train.py
@@ -1,9 +1,25 @@
 from models import *
 import numpy as np
+from glob import glob
 
 if __name__ == '__main__':
     model_name = TFU_model_2
     model = model_name.model()
     model.summary()
     model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-    model.fit()
+
+    file_list = sorted(glob("data/LibriSpeechProcessed/*.npy"))
+    dev_ssn_3, dev_clean,test_ssn_3, test_clean = file_list
+    # dev_ssn_3 = np.load(dev_ssn_3, allow_pickle=True)
+    # dev_clean = np.load(dev_clean, allow_pickle=True)
+    test_ssn_3 = np.load(test_ssn_3, allow_pickle=True)
+    test_clean = np.load(test_clean, allow_pickle=True)
+    for epoch in range(10):
+        for n, file in enumerate(test_ssn_3):
+            history = model.fit(
+                test_ssn_3[n], test_clean[n],
+                epochs=1)
+            if n % 100 == 0:
+                print("n = ", n, "; epoch = ", epoch)
+                model.save_weights('./checkpoints/my_checkpoint')
+
