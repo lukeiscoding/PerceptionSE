20a45cc3c77c8bb0b96c5dd745cf4e4d01b478a2
Shuyu Gong <sg699317@ohio.edu>
2020-07-21 20:26:30 -0400

    added stft test

---
+++ stft_test.py
@@ -0,0 +1,33 @@
+from scipy.signal import stft, istft
+from glob import glob
+import soundfile as sf
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+clean_files = sorted(glob('stft_test_file/*.flac'))
+
+# test for stft and istft
+print("full complex number")
+for file in clean_files:
+    audio, _s = sf.read(file)
+    audio = np.asarray(audio)
+    _t, _f, Zxx = stft(audio, window='hamming', nperseg=256, noverlap=128, padded=True)
+    _t, audio_recovered = istft(Zxx, window='hamming', nperseg=256, noverlap=128)
+    audio_recovered = np.asarray(audio_recovered[:len(audio)])
+    assert audio.all() == audio_recovered.all()
+    print(file, "lossless")
+
+# test for mag only
+print("mag only")
+mse = np.array([])
+for file in clean_files:
+    audio, _s = sf.read(file)
+    audio = np.asarray(audio)
+    _t, _f, Zxx = stft(audio, window='hamming', nperseg=256, noverlap=128, padded=True)
+    _t, audio_recovered = istft(np.abs(Zxx), window='hamming', nperseg=256, noverlap=128)
+    audio_recovered = np.asarray(audio_recovered[:len(audio)])
+    mse = np.append(mse, mean_squared_error(audio, audio_recovered))
+    print(file, 'error=', mse[-1])
+    sf.write(file+'_mag_only.wav', audio_recovered, _s)
+print('average mse=', mse.sum() / len(mse))
+
