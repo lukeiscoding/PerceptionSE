68f48597c9e3d7016be0eafc2e057346e2c37580
Shuyu Gong <sg699317@ohio.edu>
2020-07-22 10:37:23 -0400

    added stft test

--- stft_test.py
+++ stft_test.py
@@ -17,8 +17,8 @@ for file in clean_files:
     assert audio.all() == audio_recovered.all()
     print(file, "lossless")
 
-# test for mag only
-print("mag only")
+# test for mag only, hamming
+print("mag only, hamming")
 mse = np.array([])
 for file in clean_files:
     audio, _s = sf.read(file)
@@ -31,3 +31,33 @@ for file in clean_files:
     sf.write(file+'_mag_only.wav', audio_recovered, _s)
 print('average mse=', mse.sum() / len(mse))
 
+
+# test for mag only, hann
+print("mag only, hann")
+mse = np.array([])
+for file in clean_files:
+    audio, _s = sf.read(file)
+    audio = np.asarray(audio)
+    _t, _f, Zxx = stft(audio, nperseg=256, noverlap=128, padded=True)
+    _t, audio_recovered = istft(np.abs(Zxx), nperseg=256, noverlap=128)
+    audio_recovered = np.asarray(audio_recovered[:len(audio)])
+    mse = np.append(mse, mean_squared_error(audio, audio_recovered))
+    print(file, 'error=', mse[-1])
+    sf.write(file+'_mag_only_hann.wav', audio_recovered, _s)
+print('average mse=', mse.sum() / len(mse))
+
+
+
+# test for mag only, hann, using
+# print("mag only, hann")
+# mse = np.array([])
+# for file in clean_files:
+#     audio, _s = sf.read(file)
+#     audio = np.asarray(audio)
+#     _t, _f, Zxx = stft(audio, nperseg=256, noverlap=128, padded=True)
+#     _t, audio_recovered = istft(np.abs(Zxx), nperseg=256, noverlap=128)
+#     audio_recovered = np.asarray(audio_recovered[:len(audio)])
+#     mse = np.append(mse, mean_squared_error(audio, audio_recovered))
+#     print(file, 'error=', mse[-1])
+#     sf.write(file+'_mag_only_hann.wav', audio_recovered, _s)
+# print('average mse=', mse.sum() / len(mse))
